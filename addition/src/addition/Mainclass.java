eb1815674733e11bba96973de007d6a866b15d6e
DELL <DELL@192.168.1.11>
2020-09-24 10:54:50 +0530

    attempt to make war file

--- addition/src/addition/Mainclass.java
+++ addition/src/addition/Mainclass.java
@@ -2,7 +2,7 @@ package addition;
 
 public class Mainclass {
 	public static void main(String[] args) {
-		System.out.println("Hey");
+		System.out.println("Hey trying to make war file");
 		System.out.println("Program to add two number");
 		int a=4,b=2;
 		System.out.println("Sum of the numbers you entered is :"+(a+b));
