4460f626a468512a3a64c3939115f2ac61d435a7
Sidharth Vijayakumar <71686422+sidharthvijayakumar@users.noreply.github.com>
2021-05-27 23:35:44 +0530

    Update Mainclass.java

--- addition/src/addition/Mainclass.java
+++ addition/src/addition/Mainclass.java
@@ -4,7 +4,6 @@ public class Mainclass {
 	public static void main(String[] args) {
 		System.out.println("Hey trying to make war file");
 		System.out.println("Program to add two number");
-		System.out.println("Program to add two number");
 		int a=4,b=2;
 		System.out.println("Sum of the numbers you entered is :"+(a+b));
 		
